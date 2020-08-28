bf881afb3e5d99041593c48be3549719d4210e48
Francisco Politi <ppoliti8@gmail.com>
2020-08-28 11:53:33 +0200

    Updates

---
+++ precache-manifest.a44b68a974b3bac5d2d5c2a60ad60204.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b7f0c763d3fef20af1e1182fefd765",
+    "url": "/panchopoliti/browser-question-game/index.html"
+  },
+  {
+    "revision": "2a145acf7ff7b20206d6",
+    "url": "/panchopoliti/browser-question-game/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "8c7e7a81eb8ea1338c0a",
+    "url": "/panchopoliti/browser-question-game/static/css/main.bb1b6248.chunk.css"
+  },
+  {
+    "revision": "2a145acf7ff7b20206d6",
+    "url": "/panchopoliti/browser-question-game/static/js/2.8612734c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panchopoliti/browser-question-game/static/js/2.8612734c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7e7a81eb8ea1338c0a",
+    "url": "/panchopoliti/browser-question-game/static/js/main.f3b06f18.chunk.js"
+  },
+  {
+    "revision": "ae5595719472f5c2b1c0",
+    "url": "/panchopoliti/browser-question-game/static/js/runtime-main.2312e0ae.js"
+  },
+  {
+    "revision": "df790f72c958656ab995b3530b6d438c",
+    "url": "/panchopoliti/browser-question-game/static/media/chevron-down-solid.df790f72.svg"
+  },
+  {
+    "revision": "b2498aea33174c5d1b63ba1fd75be2b5",
+    "url": "/panchopoliti/browser-question-game/static/media/chevron-up-solid.b2498aea.svg"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/panchopoliti/browser-question-game/static/media/cog-solid.9f07a3df.svg"
+  },
+  {
+    "revision": "1ebbbd246a41c4472b84a1bb74fa6411",
+    "url": "/panchopoliti/browser-question-game/static/media/trophy-solid.1ebbbd24.svg"
+  }
+]);
